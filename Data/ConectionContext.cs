1754edaadc39be87bd9cc4c37e7d7375d5dfe305
Luciano Carlos Silva Pinto <61012376+lucianocarloss@users.noreply.github.com>
2023-01-27 20:24:12 -0300

    CRUD Person

---
+++ Data/ConectionContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using startehouse.api.Model;
+
+namespace startehouse.api.Data
+{
+	public class ConectionContext: DbContext
+	{
+		public DbSet<Person> Person { get; set; }
+
+
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			=> optionsBuilder.UseMySQL("server=localhost;database=start_house;user=root;password=root"); 
+	}
+}
+
